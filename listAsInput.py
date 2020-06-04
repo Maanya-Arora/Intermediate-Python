9a7ad8783a8f34869e8a18e33408695197ccf54b
maanya arora <40130525+Maanya-Arora@users.noreply.github.com>
2020-06-04 11:11:05 -0700

    takes list as input and prints list as output

---
+++ listAsInput.py
@@ -0,0 +1,6 @@
+listoutput = []
+n = int(input("how many elements do you want your list to have:"))
+for i in range(0,n):
+    listinput = (input("enter an item:"))
+    listoutput += listinput
+print(listoutput)
