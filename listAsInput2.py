392d0a6ad9dc0bc850863a4823b2d35c16ed99e4
maanya arora <40130525+Maanya-Arora@users.noreply.github.com>
2020-06-17 20:06:26 -0700

    edit

--- listAsInput2.py
+++ listAsInput2.py
@@ -4,9 +4,13 @@ for i in range(n):
     listinput = (input("enter an item:"))
     listoutput.append(listinput)
 
+
 listoutput.insert(n+1, "38")
 listoutput.insert(0,"36")
 listoutput.sort()
 listoutput.extend("4")
+
+for i in listoutput:
+    print(i)
 
 print(listoutput)
